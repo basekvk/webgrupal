64389e6ab65967128bd56868249d197f974083e6
David <basek32@gmail.com>
2022-11-08 20:30:37 +0100

    bundle

--- next.config.js
+++ next.config.js
@@ -6,4 +6,9 @@ const nextConfig = {
     optimizeFonts: true,
 };
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer({});
+
 module.exports = nextConfig;
