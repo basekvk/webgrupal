923bfef622bdbadcf20750f2fbb677a120834a0c
basekvk <basek32@gmail.com>
2022-12-30 13:35:58 +0100

    favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -73,7 +73,7 @@ function MyApp({ Component, pageProps }) {
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 />
-                <link rel="icon" href="/favicon.png" />
+                <link rel="icon" href="/favicon.ico" />
                 <GoogleAnalytics
                     gaMeasurementId="G-0PHDQG7XCS"
                     trackPageViews
